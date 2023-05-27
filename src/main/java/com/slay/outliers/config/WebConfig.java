08b35bbf035a5073401c285fcebb9852369a4490
JohnPrk <uo3641493@gmail.com>
2023-05-27 21:49:27 +0900

    chore : CORS 주소 이전 세팅

--- src/main/java/com/slay/outliers/config/WebConfig.java
+++ src/main/java/com/slay/outliers/config/WebConfig.java
@@ -1,15 +1,18 @@
 package com.slay.outliers.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080")
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
-
+                .allowedOrigins("https://www.slayoutliers.click")
+                .allowedHeaders("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true);
     }
 }
